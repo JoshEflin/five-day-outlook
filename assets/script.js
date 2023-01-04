16654858537962938300ca78994461079bf9b10b
ragingpenguin <eflinjh@gmail.com>
2023-01-04 16:36:36 -0500

    added in new search function, still has erros but basically works

--- assets/script.js
+++ assets/script.js
@@ -37,24 +37,64 @@
 // event listener for each appended aside to run the main fetch request function above
 
 
+// BUGS- form input does not allow for state code or country code
 
-function citySearch() {
-    var apiKey = "93a9448c198f02ecbe576b63b0dc64b3";
-fetch(requestUrl)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data)
-})
+var formInput = document.querySelector(".city-name")
+var searchHistory = document.querySelector(".search-history")
+var apiKey = "93a9448c198f02ecbe576b63b0dc64b3";
+getSearchHistory()
+
+function main() {
+    newSearch(formInput.value)
+    forecast()
 }
-function storeSearchHistory() {}
+function newSearch(userCity) {
+    
+    var geoRequestUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+userCity+"&limit=&appid="+apiKey;
+    fetch(geoRequestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) { 
+        console.log(data)  
+        var latitude = data[0].lat;
+        var longitude = data[0].lon
+    // create a local storage object with the latitude and longitude inside
+        localStorage.setItem(userCity, JSON.stringify({
+            latitude: latitude,
+            longitude: longitude,
+        }))
+    })
+    
+    var coordinates =JSON.parse(localStorage.getItem(formInput.value))
+    return coordinates;
+
+}
 // use local storage to store the search city name only, then grab the city name and perform a new fetch with current data
 
-function forcast() {}
+function forecast() {
+    // getWeathers current value is a placeholder for now
+    var getWeather = newSearch(formInput.value)
+    console.log(getWeather)
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+getWeather.latitude+"&lon="+getWeather.longitude+"&appid="+apiKey;
+    fetch(requestUrl)
+    .then(function (response){
+        return response.json()
+    })
+    .then(function(data){
+         console.log(data)
+    })
+}
 // parse json data  and append to screen  the current weather as well as 5 day outlook
 // (consider storing a prototype of JSON info in storage to use a template for faster reloading of old searches???)
-function getSearchHistory() {}
+function getSearchHistory() {
+    console.log(localStorage)
+    var cityEl = document.createElement('div')
+    console.log(localStorage.boston)
+    cityEl.innerText=localStorage.boston
+    searchHistory.appendChild(cityEl)
+    
+}
 // get local storage items and append them to the aside HTML element
 // clicking on them performs a NEW GET request (fetch from server)
 
@@ -65,4 +105,11 @@ var city;
 
 
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={}
-console.log(apiKey)
+// console.log(apiKey)
+
+formInput.addEventListener('keydown', function(event){
+    if (event.key === "Enter"){
+        main();
+    }
+    
+})
